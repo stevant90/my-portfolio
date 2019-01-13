191ccf3485033412c75bb8de8fa65531881d915f
stevant90 <stevann90@gmail.com>
2019-01-13 11:06:42 +0100

    Updates

---
+++ precache-manifest.42b52a3a76492f090d1e6a8510e5e783.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d783f6d407264b8e4f520ea0ef3cb240",
+    "url": "/my-portfolio/static/media/react.d783f6d4.png"
+  },
+  {
+    "revision": "ae1b67af034f6985eaf0",
+    "url": "/my-portfolio/static/css/main.69215887.chunk.css"
+  },
+  {
+    "revision": "fa4c82304ab42f009ca7",
+    "url": "/my-portfolio/static/js/1.fa4c8230.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "52f118b3322981d8ce2ffb917b7394d7",
+    "url": "/my-portfolio/static/media/me.52f118b3.jpg"
+  },
+  {
+    "revision": "9cf4a14cb5024b5d6e988c1519570ce3",
+    "url": "/my-portfolio/static/media/html.9cf4a14c.png"
+  },
+  {
+    "revision": "5d1cc864ae7eebae63a1c5738b446e3d",
+    "url": "/my-portfolio/static/media/css.5d1cc864.png"
+  },
+  {
+    "revision": "ae1b67af034f6985eaf0",
+    "url": "/my-portfolio/static/js/main.ae1b67af.chunk.js"
+  },
+  {
+    "revision": "b51d1861e7fe5782d20d5455c7c9f1f4",
+    "url": "/my-portfolio/static/media/redux.b51d1861.png"
+  },
+  {
+    "revision": "450c112d0d32c82bd89919ff36cb79ca",
+    "url": "/my-portfolio/static/media/jest.450c112d.png"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/my-portfolio/static/media/bootstrap-stack.73844123.png"
+  },
+  {
+    "revision": "64f68b222f5d8f40dbf5dcf0441e059c",
+    "url": "/my-portfolio/static/media/sass.64f68b22.png"
+  },
+  {
+    "revision": "0261db2fe867bd10fa2491abb95bbae0",
+    "url": "/my-portfolio/static/media/vsCode.0261db2f.png"
+  },
+  {
+    "revision": "168dd07db1a6a45a2ec75b2d899c886d",
+    "url": "/my-portfolio/index.html"
+  }
+];
